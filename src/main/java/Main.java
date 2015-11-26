2a9137cd1bb4a27577b4bbe598df2d9ba0ae7917
Nicolas Svirchevsky <n.svirchevsky@gmail.com>
2015-11-26 15:54:27 +0100

    Implemented gradle

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by svirchevsky on 26/11/15.
+ */
+public class Main {
+
+    public static void main(String[] argv) {
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by svirchevsky on 26/11/15.
+ */
+public class MainTest {
+
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
