d81f7a502938d8235d9a905bf442bc28531be45a
damiensauv <damiensauvalle@gmail.com>
2015-12-08 20:17:04 +0100

    add pluginListener

---
+++ src/main/java/files/PluginListener.java
@@ -0,0 +1,20 @@
+package files;
+
+import java.io.File;
+
+/**
+ * Created by damien on 08/12/15.
+ */
+public class PluginListener {
+
+    PluginListener(){
+    }
+
+    public void insertPlugin(File file) {
+
+    }
+
+    public void deletePlugin(File file) {
+
+    }
+}
