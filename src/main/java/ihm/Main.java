2d3f29ed52c1f86fa050a0f98a82e75545d40c74
damiensauv <damiensauvalle@gmail.com>
2015-12-09 00:07:55 +0100

    check in main if directory dropins exist

--- src/main/java/ihm/Main.java
+++ src/main/java/ihm/Main.java
@@ -5,6 +5,10 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         File directory = new File("./dropins");
+        if (!directory.exists() || !directory.isDirectory()){
+            System.out.println("Directory ./dropins missing");
+            System.exit(0);
+        }
         AppFrame frame = new AppFrame(directory);
         frame.setVisible(true);
     }
