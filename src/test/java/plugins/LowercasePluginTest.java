1fec36a1f277f8f3207c4cbcaa9a38f625d0e07c
Laurent THIEBAULT <thiebault.laurent@gmail.com>
2015-12-10 00:05:11 +0100

    Ajout tests plugins

--- src/test/java/plugins/LowercasePluginTest.java
+++ src/test/java/plugins/LowercasePluginTest.java
@@ -1,10 +1,26 @@
 package plugins;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-/**
- * Created by damien on 09/12/15.
- */
 public class LowercasePluginTest {
 
+    protected LowercasePlugin lowercasePlugin;
+
+    @Before
+    public void init() {
+        lowercasePlugin = new LowercasePlugin();
+    }
+
+    @Test
+    public void testDescription() {
+        assertEquals("En minuscule", lowercasePlugin.getDescription());
+    }
+
+    @Test
+    public void testDoAction() {
+        assertEquals("test", lowercasePlugin.doAction("Test"));
+    }
 }
